dbf1e9e7d9b199b2c774609ebe5d05f7b228ec65
Plutorian-Programmer <karsten.langerak@gmail.com>
2023-01-19 15:10:24 +0100

    update

--- args.py
+++ args.py
@@ -8,4 +8,9 @@ def arg_parser_preprocessing():
                         help="path to original review data")
     parser.add_argument("--user_thresh", dest="user_thresh", type=int, default=5, 
                         help="remove users with reviews less than this threshold")
+    parser.add_argument("--item_thresh", dest="item_thresh", type=int, default=5, 
+                        help="remove users with reviews less than this threshold")
+    parser.add_argument("--test_length", dest="test_length", type=int, default=5, 
+    help="the number of test items")
+    parser.add_argument("--neg_length", dest="neg_length", type=int, default=100, help="# of negative samples in evaluation")
     return parser.parse_args()
