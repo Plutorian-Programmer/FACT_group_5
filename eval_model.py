57ac084078c8ac50ca913695b0d6bd827e8b6272
w1etse <92383148+w1etse@users.noreply.github.com>
2023-01-20 15:28:35 +0100

    Begin optimalization
    mooi mooi

--- eval_model.py
+++ eval_model.py
@@ -5,21 +5,21 @@ from args import *
 import pickle
 
 device = 'cpu'
-dataset_path = "models\Dataset.pickle"
+dataset_path = "models\Dataset_20.pickle"   #only 20
 with open(dataset_path, "rb") as f:
     rec_dataset = pickle.load(f)
 
 model_path = "models\model.model"
 model = BaseRecModel(rec_dataset.feature_num, rec_dataset).to(device)
 model.load_state_dict(torch.load(model_path))
-k = 5
+k = 20
 f1_score = compute_f1(rec_dataset.test_data, 
             rec_dataset.user_feature_matrix, 
             rec_dataset.item_feature_matrix, 
             k, 
             model, 
             device)
-ndcg_score = compute_ndcg(rec_dataset.test_data, 
+ndcg_score = compute_ndcg(rec_dataset.test_data,    
             rec_dataset.user_feature_matrix, 
             rec_dataset.item_feature_matrix, 
             k, 
--- train_base.py
+++ train_base.py
@@ -80,3 +80,29 @@ if __name__ == "__main__":
     train_args = arg_parser_training()
     pre_processing_args = arg_parser_preprocessing()
     trainmodel(train_args, pre_processing_args)
+
+# We have two groups of users, g0 and g1, and we want to recommend items to them.
+# exposure of recommendation model g is the number of items in R that g0 has interacted with.
+
+def exposure(g, R, user_history):
+    g_exposure = 0
+    for user in R:
+        if user_history[user][0] == g:
+            g_exposure += 1
+    return g_exposure
+
+# calculate a quantification measure for diparity
+def disparity(R, user_history):
+    g0_exposure = exposure(0, R, user_history)
+    g1_exposure = exposure(1, R, user_history)
+
+    # take the difference between the two sides of the equalities as a quantification measure for disparity
+    # theta_DP = abs(G1) * Exposure (G0 |R𝐾) − abs(G0) * Exposure (G1 |R𝐾)
+    ##### weet niet hoe ik de absolute waarde van g0 moet nemen, is dat de totale lengte van de lijst?
+    theta_DP = abs(g1_exposure) * g0_exposure - abs(g0_exposure) * g1_exposure
+    return theta_DP
+
+#Counterfactual reasoning
+
+
+
