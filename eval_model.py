43f93816222a06bec814e586d7fb2d6f0b124de2
JonasElburgUVA <114097649+JonasElburgUVA@users.noreply.github.com>
2023-01-21 12:55:58 +0100

    bugfix eval_model

--- eval_model.py
+++ eval_model.py
@@ -5,11 +5,11 @@ from args import *
 import pickle
 
 device = 'cpu'
-dataset_path = "models\Dataset_20.pickle"
+dataset_path = "models/Dataset_20.pickle"
 with open(dataset_path, "rb") as f:
     rec_dataset = pickle.load(f)
 
-model_path = "models\model_20.model"
+model_path = "models/model_20.model"
 model = BaseRecModel(rec_dataset.feature_num, rec_dataset).to(device)
 model.load_state_dict(torch.load(model_path))
 k = 5
@@ -26,5 +26,5 @@ ndcg_score = compute_ndcg(rec_dataset.test_data,
             model, 
             device)
 
-print(f1_score)
-print(ndcg_score)
+print(f"F1: {f1_score}")
+print(f"NDCG: {ndcg_score}")
